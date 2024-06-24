ff40fa2a187e1442e7b484f859d7511a544f63cf
Alexandre-verbreugh <averbreugh@utopiaconsulting.fr>
2024-06-25 00:16:54 +0200

    Live Component

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class BlogController extends AbstractController
+class SearchController extends AbstractController
 {
-    #[Route('/', name: 'app_blog')]
+    #[Route('/search', name: 'app_search')]
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig');
+        return $this->render('search/index.html.twig');
     }
 }
